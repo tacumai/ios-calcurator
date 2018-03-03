699a58908f2d128653ffeb4e621739ae7701a446
tacmac <tacuma.igei@gmail.com>
2018-03-04 01:18:25 +0900

    refactor

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -35,31 +35,7 @@ class ViewController: UIViewController {
     }
 
     @IBAction func numberButtonTapped(_ sender: UIButton) {
-        var value = 0
-        switch sender.currentTitle! {
-        case "0":
-            value = 0
-        case "1":
-            value = 1
-        case "2":
-            value = 2
-        case "3":
-            value = 3
-        case "4":
-            value = 4
-        case "5":
-            value = 5
-        case "6":
-            value = 6
-        case "7":
-            value = 7
-        case "8":
-            value = 8
-        case "9":
-            value = 9
-        default:
-            value = 0
-        }
+        let value = Int(sender.currentTitle!)!
         if currentOperator == .undefined {
             firstValue = firstValue * 10 + value
             label.text = "\(firstValue)"
